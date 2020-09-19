45f1f14a1ffd571a58d30899d39f5dc6274b728c
chaizhiyang <18291907191@163.com>
2020-09-19 18:20:19 +0800

    fix bugs

--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -1,11 +1,11 @@
 <!--
  * @Date: 2020-09-19 15:12:21
  * @LastEditors: chaizhiyang(狗尾草)
- * @LastEditTime: 2020-09-19 15:12:49
+ * @LastEditTime: 2020-09-19 18:15:02
  * @Description: please write description
 -->
 <template>
-  <div>这里是用户模块</div>
+  <div class="test">这里是用户模块</div>
 </template>
 
 <script>
@@ -15,5 +15,7 @@ export default {
 </script>
 
 <style lang="less" scoped>
-
+.test {
+  background-color: @bg-blue;
+}
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,10 @@
 /**
 * Date: 2020-09-19 09:55:30
 * LastEditors: chaizhiyang(狗尾草)
-* LastEditTime: 2020-09-19 11:12:11
+* LastEditTime: 2020-09-19 18:19:38
 * Description: please write description
 */
+var path = require('path');
 module.exports = {
   devServer: {
     proxy: {
@@ -16,5 +17,20 @@ module.exports = {
       }
     },
     before: process.env.NODE_ENV == 'dev' ? require('./mock/mock-server.js') : null
-  }
+  },
+  lintOnSave: false,
+  chainWebpack: config => {
+    const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
+    types.forEach(type => addStyleResource(config.module.rule('less').oneOf(type)))
+  },
+}
+
+function addStyleResource (rule) {
+  rule.use('style-resource')
+    .loader('style-resources-loader')
+    .options({
+      patterns: [
+        path.resolve(__dirname, './src/styles/variable.less'),
+      ],
+    })
 }
